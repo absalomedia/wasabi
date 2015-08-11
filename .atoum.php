ad5ddeb99ce3aabb939f780fae343334dd524e9c
Lawrence <media@absalom.biz>
2015-08-11 14:10:29 +1000

    Atoum

---
+++ .atoum.php
@@ -0,0 +1,32 @@
+<?php
+use mageekguy\atoum\reports;
+$runner
+	->addTestsFromDirectory(__DIR__ . '/tests')
+	->disallowUsageOfUndefinedMethodInMock()
+;
+$runner->getScore()->getCoverage();
+$travis = getenv('TRAVIS');
+if ($travis)
+{
+	$script->addDefaultReport();
+	$coverallsToken = getenv('COVERALLS_REPO_TOKEN');
+	if ($coverallsToken)
+	{
+		$coverallsReport = new reports\asynchronous\coveralls('classes', $coverallsToken);
+		$defaultFinder = $coverallsReport->getBranchFinder();
+		$coverallsReport
+			->setBranchFinder(function() use ($defaultFinder) {
+					if (($branch = getenv('TRAVIS_BRANCH')) === false)
+					{
+						$branch = $defaultFinder();
+					}
+					return $branch;
+				}
+			)
+			->setServiceName('travis-ci')
+			->setServiceJobId(getenv('TRAVIS_JOB_ID'))
+			->addDefaultWriter()
+		;
+		$runner->addReport($coverallsReport);
+	}
+}
