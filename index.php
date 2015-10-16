1e4cd971af71d323c6b651fdffd217e62ef3479a
Lawrence <media@absalom.biz>
2015-10-16 14:45:27 +1000

    More websocket namespaces - cart & product

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ use Ratchet\WebSocket\WsServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
 use WS\Wasabi\Combination;
+use WS\Wasabi\Product;
+use WS\Wasabi\Cart;
 
     $wasabi = new WsServer(new Combination);
     $wasabi->disableVersion(0); // old, bad, protocol version
