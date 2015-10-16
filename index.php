1f21c8507de2d381d702f54a186900b9e1ceb0f2
Lawrence <media@absalom.biz>
2015-10-16 14:49:48 +1000

    Unique ports / servers for each process

--- index.php
+++ index.php
@@ -20,3 +20,20 @@ use WS\Wasabi\Cart;
     // Make sure you're running this as root
     $server = IoServer::factory(new HttpServer($wasabi), 8787,  '0.0.0.0');
     $server->run();
+
+
+    $wasabi2 = new WsServer(new Product);
+    $wasabi2->disableVersion(0); // old, bad, protocol version
+    $wasabi2->setEncodingChecks(false);
+
+    // Make sure you're running this as root
+    $server2 = IoServer::factory(new HttpServer($wasabi2), 8788,  '0.0.0.0');
+    $server2->run();
+
+    $wasabi3 = new WsServer(new Cart);
+    $wasabi3->disableVersion(0); // old, bad, protocol version
+    $wasabi3->setEncodingChecks(false);
+
+    // Make sure you're running this as root
+    $server3 = IoServer::factory(new HttpServer($wasabi3), 8789,  '0.0.0.0');
+    $server3->run();  
